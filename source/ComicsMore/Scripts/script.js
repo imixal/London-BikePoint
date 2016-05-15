8cb18e826c84b5718f18cf01cad1e27b7a04115a
Duke <duknikta@gmail.com>
2016-05-15 14:12:49 +0300

    comics  updates

--- source/ComicsMore/Scripts/script.js
+++ source/ComicsMore/Scripts/script.js
@@ -30,26 +30,26 @@
         $(".modal-window").hide("fast");
     }
     function cell(celling) {
-        switch (celling) {
+        switch (celling.style) {
             case "cell-1": {
                 $("#page-inner-" + current_page_number).remove();
                 $("#page-" + current_page_number).append('<div id="page-inner-' + current_page_number + '"><div class="template-tab"></div></div>');
-                $(".template-tab").css({ "width": $("#page-" + current_page_number).css("width"), "height": $("#page-" + current_page_number).css("height"), "border": "solid " + $("input[name=color]").val() + " " + $("input[name=weight]").val() + "px" });
-                page[current_page_number].template = "cell-1";
+                $(".template-tab").css({ "width": $("#page-" + current_page_number).css("width"), "height": $("#page-" + current_page_number).css("height"), "border": "solid " + celling.color+ " " +celling.weight + "px" });
+                page[current_page_number].template.style = "cell-1";
                 $("#cell-1").prop("checked", true);
                 break
             }
             case "cell-2": { $("#page-inner-" + current_page_number).remove();
                 $("#page-" + current_page_number).append('<div id="page-inner-' + current_page_number + '"><div class="template-tab"></div><div class="template-tab"></div></div>');
-                $(".template-tab").css({ "width": $("#page-" + current_page_number).css("width"), "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 2 + "px", "border": "solid " + $("input[name=color]").val() + " " + $("input[name=weight]").val() + "px" });
-                page[current_page_number].template = "cell-2";
+                $(".template-tab").css({ "width": $("#page-" + current_page_number).css("width"), "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 2 + "px", "border": "solid " + celling.color + " " + celling.weight + "px" });
+                page[current_page_number].template.style = "cell-2";
                 $("#cell-2").prop("checked", true);
                 break
             }
             case "cell-4": { $("#page-inner-" + current_page_number).remove();
                 $("#page-" + current_page_number).append('<div id="page-inner-' + current_page_number + '"><div class="template-tab"></div><div class="template-tab"></div><div class="template-tab"></div><div class="template-tab"></div></div>');
-                $(".template-tab").css({ "width": Number($("#page-" + current_page_number).css("width").substring(0, 3)) / 2 + "px", "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 2 + "px", "border": "solid " + $("input[name=color]").val() + " " + $("input[name=weight]").val() + "px" });
-                page[current_page_number].template = "cell-4";
+                $(".template-tab").css({ "width": Number($("#page-" + current_page_number).css("width").substring(0, 3)) / 2 + "px", "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 2 + "px", "border": "solid "  +celling.color + " " + celling.weight + "px" });
+                page[current_page_number].template.style = "cell-4";
                 $("#cell-4").prop("checked", true);
                 break
             }
@@ -57,14 +57,14 @@
                 $("#page-inner-" + current_page_number).remove();
                 $("#page-" + current_page_number).append('<div id="page-inner-' + current_page_number + '"><div class="template-tab-incline"></div><div class="template-tab-incline">  </div></div>');
                 if ($("#A4a").is(":checked")) {
-                    $(".template-tab-incline").css({ "width": Number($("#page-" + current_page_number).css("width").substring(0, 3)) * 3 + "px", "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 3 * 2 + "px", "border-bottom": "solid " + $("input[name=color]").val() + " " + $("input[name=weight]").val() + "px" });
+                    $(".template-tab-incline").css({ "width": Number($("#page-" + current_page_number).css("width").substring(0, 3)) * 3 + "px", "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 3 * 2 + "px", "border-bottom": "solid " + celling.color + " " + celling.weight + "px" });
                 }
                 if ($("#A4b").is(":checked")) {
-                    $(".template-tab-incline").css({ "width": Number($("#page-" + current_page_number).css("width").substring(0, 3)) * 3 + "px", "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 4 * 3 + "px", "border-bottom": "solid " + $("input[name=color]").val() + " " + $("input[name=weight]").val() + "px" });
+                    $(".template-tab-incline").css({ "width": Number($("#page-" + current_page_number).css("width").substring(0, 3)) * 3 + "px", "height": Number($("#page-" + current_page_number).css("height").substring(0, 3)) / 4 * 3 + "px", "border-bottom": "solid " + celling.color + " " + celling.weight + "px" });
 
                 }
                 $("#incline-cell").prop("checked", true);
-                page[current_page_number].template = "cell-incline";
+                page[current_page_number].template.style = "cell-incline";
                 break
             }
         }
@@ -73,17 +73,22 @@
     }
     function template() {
         if ($("#cell-1").is(":checked")) {
-            cell("cell-1");
+            page[current_page_number].template.style = "cell-1";
+            cell(page[current_page_number].template);
         }
         if ($("#cell-2").is(":checked")) {
-            cell("cell-2");
+            page[current_page_number].template.style = "cell-2";
+            cell(page[current_page_number].template);
         }
         if ($("#cell-4").is(":checked")) {
-            cell("cell-4");
+            page[current_page_number].template.style = "cell-4";
+            cell(page[current_page_number].template);
         }
         if ($("#incline-cell").is(":checked")) {
-            cell("cell-incline");
+            page[current_page_number].template.style = "cell-incline";
+            cell(page[current_page_number].template);
         }
+        addtexts();
     };
     function page_format(format) {
         $("#page-" + current_page_number).css({ "overflow": "hidden", "position": "absolute", "top": "50px", "left": "310px", "background-color": "#fff" })
@@ -108,11 +113,21 @@
         if ($("#A4b").is(":checked")) {
             page_format(0);
         }
-       
+        addtexts();
     });
     $(function () { $("input[name=template]").change(function () { template(); }) })
-    $(function () { $("input[name=weight]").change(function () { template(); }) });
-    $(function () { $("input[name=color]").change(function () { template(); }) });
+    $(function () {
+        $("input[name=weight]").change(function () {
+            page[current_page_number].template.weight = $("input[name=weight]").val();
+            template();
+        })
+    });
+    $(function () {
+        $("input[name=color]").change(function () {
+            page[current_page_number].template.color = $("input[name=weight]").val();
+            template();
+        })
+    });
 
     $("#tool-page").click(function () {
         close();
@@ -134,11 +149,6 @@
         $("#modal-window-video").stop().toggle("fast");
         active($(this));
     });
-    $("#tool-comment").click(function () {
-        close();
-        $("#modal-window-comment").stop().toggle("fast");
-        active($(this));
-    });
     $("#tool-font").click(function () {
         close();
         $("#modal-window-font").stop().toggle("fast");
@@ -151,11 +161,18 @@
     });
     function addtext(text) {
         var temp = gen_text;
-        $("#page-inner-" + current_page_number).children().first().append('<div class="draggable generate-text" id="' + temp + '">' + text.value + '</div>');
-        $("#" + temp).css({ "font-size": text.size + "px", "color": text.color + "px", "font-weight": text.weight,"left" :text.left,"top": text.top })
+        $("#page-inner-" + current_page_number).children().first().append('<div id="t' + temp + '" class="draggable generate-text ' + text.type_cloud + ' speech-bubble-' + text.side_cloud + '" >' + text.value + '</div>');
+        $("#t" + temp).css({"width":text.width+"px","height":text.height+"px","line-height":text.height+"px", "font-size": text.size + "px", "color": text.color + "px", "font-weight": text.weight,"left" :text.left,"top": text.top })
         drags();
         gen_text++;
     };
+    function addtexts() {
+        if (Object.keys(page[current_page_number].text).length != 0) {
+            for (var item in page[current_page_number].text) {
+                addtext(page[current_page_number].text[item]);
+            }
+        }
+    }
     /* картинка и облако*/
     $("#generate-btn").click(function () {
             page[current_page_number].text[$("#textholder-generate").val()] = {
@@ -165,6 +182,10 @@
                 size: $("#size-generate").val(),
                 color: $("#color-generate").val(),
                 weight: $("#weight-generate").val(),
+                height: $("#height-generate").val(),
+                width: $("#width-generate").val(),
+                type_cloud: $("#cloud-generate").val(),
+                side_cloud: $("#side-cloud-generate").val(),
             };
             addtext(page[current_page_number].text[$("#textholder-generate").val()]);
             $(".generate-text").click(function () {
@@ -174,28 +195,18 @@
 
         
     });
-    $("#textholder-generate").change(function () {
-        $("#generate-text").empty();
-        $("#generate-text").append('<p>' + $("#textholder-generate").val() + '</p>');
-    });
-    $("#size-generate").change(function () {
-        $("#generate-text").css("font-size", $("#size-generate").val()+"px");
-    });
-    $("#color-generate").change(function () {
-        $("#generate-text").css("color", $("#color-generate").val() + "px");
-    });
-    $("#weight-generate").change(function () {
-        $("#generate-text").css("font-weight", $("#weight-generate").val());
-    });
     $("#btn-add-page").click(function () {
         $("#menu-page").append('<input id="menu-page-' + number_of_page + '" type="radio" name="pages"/><label class="pages" for="menu-page-' + number_of_page + '">' + number_of_page + '</label>');
         $("#edit-page").append('<section id="page-' + number_of_page + '"><div id="page-inner-' + number_of_page + '" >   </div></section>');
         page[number_of_page] = {
             style_page: 1,
-            template: "cell-1",
+            template: {
+                style: "cell-1",
+                weight: 5,
+                color: "",
+            },
             picture: {},
             video: {},
-            clouds: {},
             text: [],
         };
         $("#menu-page-"+number_of_page).click(function () {
@@ -204,11 +215,7 @@
             page_format(page[current_page_number].style_page);
             stop();
             cell(page[current_page_number].template);
-            if (Object.keys(page[current_page_number].text).length != 0) {
-                for (var item in page[current_page_number].text) {
-                    addtext(page[current_page_number].text[item]);
-                }
-            }
+            addtexts();
             $("#page-" + current_page_number).show();
             
         });
